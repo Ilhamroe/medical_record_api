491d775e4ddda7ef787670e70c951bc9092a02f5
iramadani2003@gmail.com <iramadani2003@gmail.com>
2024-06-06 04:41:09 +0700

    updated

--- app/Http/Controllers/Clinic/ClinicController.php
+++ app/Http/Controllers/Clinic/ClinicController.php
@@ -112,7 +112,7 @@ class ClinicController extends Controller
         Clinic::create([
             'doctor_id' => Auth::user()->id,
             'patient_id' => $validatedData['patient_id'],
-            'dated' => date('Y-m-d', strtotime($validatedData['birth'])),
+            'dated' => date('Y-m-d', strtotime($validatedData['dated'])),
             'symptom' => $validatedData['symptom'],
             'diagnosis' => $validatedData['diagnosis'],
             'drug' => $validatedData['drug'],
